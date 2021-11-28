a508465b4d335061cdb677db059ad797a9aac3f6
stas <garanzha.s.a@gmail.com>
2021-11-28 15:26:16 +0300

    Add serializer for order data

---
+++ foodcartapp/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Order, ProductQuantity
+
+
+class ProductQuantitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductQuantity
+        fields = ['product', 'quantity', ]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    products = ProductQuantitySerializer(many=True, allow_empty=False,
+                                         write_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def create(self, validated_data):
+        products = validated_data.pop('products')
+        order = Order.objects.create(**validated_data)
+        products_for_order = [
+            ProductQuantity(**product | {'order': order})
+            for product in products
+        ]
+        ProductQuantity.objects.bulk_create(products_for_order)
+        return order
