658e9c548f8a9e1b86813debd2d487ee3eb54a6e
stas <garanzha.s.a@gmail.com>
2021-11-28 15:38:02 +0300

    List orders into manager UI

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 
 
-from foodcartapp.models import Product, Restaurant
+from foodcartapp.models import Order, Product, Restaurant
 
 
 class Login(forms.Form):
@@ -97,6 +97,9 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
-    return render(request, template_name='order_items.html', context={
-        # TODO заглушка для нереализованного функционала
-    })
+    not_processed_orders = Order.objects.filter(status=2)
+    return render(
+        request,
+        template_name='restaurateur/order_items.html',
+        context={'orders': not_processed_orders}
+    )
